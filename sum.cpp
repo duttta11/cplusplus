c8c6e229572bce6acc1b5d9d0add3723170d6114
Aditya Dutta <adityadutta482@gmail.com>
2026-07-16 00:15:50 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter the num";
+    cin>>a;
+    cout<<"enter the second num";
+    cin>>b;
+    c=a+b;
+    cout<<"the sum is="<<c;
+    return 0;
+}
