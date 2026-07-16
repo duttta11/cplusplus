2dc6d1cd4eb895ada2d4526d2aa48da7a0f6da84
Aditya Dutta <adityadutta482@gmail.com>
2026-07-16 16:13:24 +0530

    Create pattern(2).cpp

---
+++ pattern(2).cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cout<<"enter the num=";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        char ch='A';
+        for(j=1;j<=n;j++){
+        cout<<ch;
+        ch=ch+1;
+    }
+    cout<<endl;}
+    return 0;
+}
